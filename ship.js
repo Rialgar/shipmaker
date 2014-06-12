20b969bd10d18c7ec9a3e3a5e5b5db9a75d13300
Rialgar <rialgar42@gmail.com>
2014-06-12 23:28:23 +0200

    refactored creating a face into a function

--- ship.js
+++ ship.js
@@ -3,87 +3,66 @@
 window.ShipGeometry = function( params ) {
 
 	THREE.Geometry.call( this );
+	
+	var scope = this;
 
 	var sqr = Math.sqrt( 0.75 ) / 3;
 
-	var A = new THREE.Vector3( 2*sqr,  0.0,  -sqr );
-	var B = new THREE.Vector3(  -sqr,  0.5,  -sqr );
-	var C = new THREE.Vector3(  -sqr, -0.5,  -sqr );
-	var D = new THREE.Vector3(   0.0,  0.0, 2*sqr );
+	this.vertices.push( new THREE.Vector3( 2*sqr,  0.0,  -sqr ) );
+	this.vertices.push( new THREE.Vector3(  -sqr,  0.5,  -sqr ) );
+	this.vertices.push( new THREE.Vector3(  -sqr, -0.5,  -sqr ) );
+	this.vertices.push( new THREE.Vector3(   0.0,  0.0, 2*sqr ) );
 
-	this.vertices.push( A );
-	this.vertices.push( B );
-	this.vertices.push( C );
-	this.vertices.push( D );
-
-	var normal = new THREE.Vector3();
-	var face, uva, uvb, uvc;
+	var uva, uvb, uvc;
 
 	//-----------------------------BOTTOM-----------------------------
-
-	normal.crossVectors(C.clone().sub(A), B.clone().sub(A));
-	normal.normalize();
-
-	face = new THREE.Face3( 0, 2, 1 );
-	face.normal.copy( normal );
-	face.vertexNormals.push( normal.clone(), normal.clone(), normal.clone() );
-	face.materialIndex = 0;
-	this.faces.push( face );
 
 	uva = new THREE.Vector2( 0.25,       0 );
 	uvb = new THREE.Vector2( 0   , sqr*3/2 );
 	uvc = new THREE.Vector2( 0.5 , sqr*3/2 );
-	this.faceVertexUvs[ 0 ].push( [ uva, uvb, uvc ] );
 
-	console.log(this);
+	addFace( 0, 2, 1, uva, uvb, uvc, 0);
 
 	//------------------------------SIDE------------------------------
-
-	normal.crossVectors(B.clone().sub(A), D.clone().sub(A));
-	normal.normalize();
-
-	face = new THREE.Face3( 0, 1, 3 );
-	face.normal.copy( normal );
-	face.vertexNormals.push( normal.clone(), normal.clone(), normal.clone() );
-	face.materialIndex = 1;
-	this.faces.push( face );
 
 	uva = new THREE.Vector2(  0.75, 0       );
 	uvb = new THREE.Vector2(  0.5 , sqr*3/2 );
 	uvc = new THREE.Vector2(  1.0 , sqr*3/2 );
-	this.faceVertexUvs[ 0 ].push( [ uva, uvb, uvc ] );
+
+	addFace( 0, 1, 3, uva, uvb, uvc, 1);
 
 	//------------------------------SIDE------------------------------
-
-	normal.crossVectors(C.clone().sub(B), D.clone().sub(B));
-	normal.normalize();
-
-	face = new THREE.Face3( 1, 2, 3 );
-	face.normal.copy( normal );
-	face.vertexNormals.push( normal.clone(), normal.clone(), normal.clone() );
-	face.materialIndex = 2;
-	this.faces.push( face );
 
 	uva = new THREE.Vector2(  0.25, 0.5           );
 	uvb = new THREE.Vector2(  0   , 0.5 + sqr*3/2 );
 	uvc = new THREE.Vector2(  0.5 , 0.5 + sqr*3/2 );
-	this.faceVertexUvs[ 0 ].push( [ uva, uvb, uvc ] );
+
+	addFace( 1, 2, 3, uva, uvb, uvc, 2);
 
 	//------------------------------SIDE------------------------------
-
-	normal.crossVectors(A.clone().sub(C), D.clone().sub(C));
-	normal.normalize();
-
-	face = new THREE.Face3( 2, 0, 3 );
-	face.normal.copy( normal );
-	face.vertexNormals.push( normal.clone(), normal.clone(), normal.clone() );
-	face.materialIndex = 3;
-	this.faces.push( face );
 
 	uva = new THREE.Vector2(  0.75, 0.5           );
 	uvb = new THREE.Vector2(  0.5 , 0.5 + sqr*3/2 );
 	uvc = new THREE.Vector2(  1.0 , 0.5 + sqr*3/2 );
-	this.faceVertexUvs[ 0 ].push( [ uva, uvb, uvc ] );
+	addFace( 2, 0, 3, uva, uvb, uvc, 3);
+
+	function addFace(a, b, c, uva, uvb, uvc, materialIndex) {
+		var A = scope.vertices[a];
+		var B = scope.vertices[b];
+		var C = scope.vertices[c];
+
+		var normal = new THREE.Vector3();
+		normal.crossVectors(B.clone().sub(A), C.clone().sub(A));
+		normal.normalize();
+
+		var face = new THREE.Face3( a, b, c );
+		face.normal.copy( normal );
+		face.vertexNormals.push( normal.clone(), normal.clone(), normal.clone() );
+		face.materialIndex = materialIndex;
+		scope.faces.push( face );
+
+		scope.faceVertexUvs[ 0 ].push( [ uva, uvb, uvc ] );
+	}
 }
 
 ShipGeometry.prototype = Object.create( THREE.Geometry.prototype );	
